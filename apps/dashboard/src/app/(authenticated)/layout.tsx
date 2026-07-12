4341669857342c0e1f5e901bd7c42d58485f76c8
jakegoodmandev <jakegoodmandev@gmail.com>
2026-07-11 21:14:25 -0400

    Clean/1 (#7)
    * rename authenticated group
    
    * Redirect to login if there is an error fetching staff or workspaces
    
    ---------
    
    Co-authored-by: jake.goodman1 <jake.goodman@route.com>

--- apps/dashboard/src/app/(authenticated)/layout.tsx
+++ apps/dashboard/src/app/(authenticated)/layout.tsx
@@ -1,14 +1,24 @@
 import { StaffProvider } from '@/components/staff-provider';
 import { getStaff } from '@/lib/data/staff';
 import { getWorkspaces } from '@/lib/data/workspace';
+import { redirect } from 'next/navigation';
 
 export default async function StaffLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const staff = await getStaff();
-  const workspaces = await getWorkspaces();
+  let staff = null;
+  let workspaces = null;
+
+  // Redirect to login if there is an error fetching staff or workspaces
+  try {
+    staff = await getStaff();
+    workspaces = await getWorkspaces();
+  } catch (error: unknown) {
+    console.error('Error fetching staff or workspaces:', error);
+    redirect('/auth/login');
+  }
 
   console.log('Rendering StaffLayout...');
   return (
